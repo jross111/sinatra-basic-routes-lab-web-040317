9ff85c3c7ba0ff81bedffa16064d61770642ed41
github username <github email address>
2017-04-24 17:26:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  # def call
+    # response = Rack::Response(env)
+    # request = Rack::Request(env)
+
+    get '/name' do
+      response.write 'My name is Slim'
+
+    end
+
+    get '/hometown' do
+      response.write 'My hometown is Detroit'
+    end
+
+    get '/favorite-song' do
+      response.write 'My favorite song is Over The Rainbow'
+    end
+  # end
+
+
 end
